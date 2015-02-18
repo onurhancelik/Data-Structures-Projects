b877e23799eee28a2316f9e55afc80e6536fa0ee
ocelik <celikonurhan@gmail.com>
2015-02-18 22:49:18 +0200

    upload

---
+++ Program 1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Proje1
+{
+    class Program
+    {
+        static Random r = new Random();
+
+        static void Main(string[] args)
+        {
+            int secim; 
+            Console.WriteLine("\n   1. İstatistiksel Sonuç Tablosu \n   2. Yıl veya yılların ay ve günlerindeki çakışmalar");
+            Console.Write("\n Seçiminizi giriniz: ");
+            do{
+                secim = Convert.ToInt16(Console.ReadLine());
+            } while (secim<1 || secim>2);
+
+            int yıl1,yıl2;
+
+            Console.WriteLine("\n Deney yapılacak yıl aralığını giriniz \n Tek bir yıl için aynı değeri yazınız. İlk olarak küçük yılı giriniz..\n");
+            Console.Write("\t>> 1. yılı giriniz: ");
+            yıl1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\t>> 2. yılı giriniz: ");
+            yıl2 = Convert.ToInt32(Console.ReadLine());
+
+            int[] yıllar = new int[yıl2 - yıl1 + 1];
+
+            int gecici = yıl1;
+            for (int i = 0; i < yıl2 - yıl1 + 1; i++)
+            {
+                yıllar[i] = gecici;
+                gecici++;
+            }
+
+            int[, ,] tarih = new int[31, 12, yıl2-yıl1+1]; //tarihleri tutan 3 boyutlu dizi oluşturuluyor.
+            
+            int[,] cakisma = new int[4,10]; //Farklı N değerleri için her 10 deneyin çakışmalarını tutan 2 boyutlu dizi oluşturuluyor.
+
+            for(int a=0; a<4; a++) //4 farklı N için ayrı ayrı deney yapılıyor.
+            {
+                int n;
+                Console.Write("\n >> "+(a+1)+". "+"N değerini giriniz: ");
+                n = Convert.ToInt16(Console.ReadLine());
+
+                for (int b = 0; b < 10; b++) //Her N değeri için 10 kere deney yapılıyor.
+                {
+                    n_rand_tarih(tarih,n,yıllar); //girilen yıl ve n değerlerine göre doğum tarihleri oluşturuluyor.
+
+                    if (secim != 1) //Eğer her deneyin ayrıntısının görülmesi isteniyorsa bu if bloğunun içine giriyor.
+                    {
+                        Console.Write("\n>>> N=" + n + " için " + (b + 1) + ". deney <<<");
+                        yazdır(tarih, yıllar);
+                        Console.ReadLine();
+                    }
+                    for (int i = 0; i < 31; i++) 
+                    {
+                        for (int j = 0; j < 12; j++)
+                        {
+                            for (int k = 0; k < yıllar.Length; k++)
+                            {
+                                if (tarih[i, j, k] > 1)
+                                    cakisma[a,b] += tarih[i, j, k] - 1; //Her tarihe ait çakışma sayıları toplanıyor.
+                            }
+                        }
+                    }
+                    Array.Clear(tarih, 0, tarih.Length); //Her deney bittikçe tarih dizisi temizleniyor.
+                }
+            }
+
+            istatistikYazdır(cakisma); //Programın en sonunda deneylerin çakışma sayıları ve ortalamaları yazdırılıyor.
+            
+            Console.ReadLine();
+        }
+
+        public static void rand_tarih(int[, ,] tarih,int[] yıllar) //Gelen yıllara göre 1 tane tarih üretiyor.
+        {
+            int ay, gün=0, yıl;
+
+            ay = r.Next(12);
+            yıl = r.Next(tarih.GetLength(2));
+
+            if (yıllar[yıl] %4==0 && ay == 1)
+                gün = r.Next(29);
+            else
+            {
+                switch (ay)
+                {
+                    case 0: case 2: case 4: case 6: case 7: case 9: case 11: gün = r.Next(31); break;
+                    case 1: gün = r.Next(28); break;
+                    case 3: case 5: case 8: case 10: gün = r.Next(30); break;
+                }
+            }
+
+            tarih[gün, ay, yıl]++;
+        }
+
+        public static void n_rand_tarih(int[, ,] tarih, int n,int[] yıllar) //n tane tarih üretiyor.
+        {
+            for (int i = 0; i < n; i++)
+            {
+                rand_tarih(tarih,yıllar);
+            }
+        }
+
+        public static void yazdır(int[, ,] tarih, int[] yıllar) //12 satırlı her günde kaç doğum tarihinin olduğunu tablo halinde yazdıran metot. 
+        {
+            int i, j, k;
+            string[] yılAdları = { "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık" };
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (k = 0; k < tarih.GetLength(2); k++)
+            {
+                Console.WriteLine("   " + yıllar[k]);
+                Console.Write("\t");
+                for (int i2 = 0; i2 < tarih.GetLength(0); i2++)
+                {
+                    if(i2>8)
+                    Console.Write((i2 + 1) + "");
+                    else
+                    Console.Write((i2 + 1) + " ");
+                }
+                Console.WriteLine();
+                for (j = 0; j < tarih.GetLength(1); j++)
+                {
+                    Console.Write(yılAdları[j] + "\t");
+
+                    for (i = 0; i < tarih.GetLength(0); i++) 
+                    {
+                        if (((i==30 && j==10)) || (yıllar[k] % 4 == 0 && j == 1 && i > 28) || (j == 1 && i > 27 && yıllar[k] % 4 != 0) || (i == 30 && (j == 3 || j == 5 || j == 8))) 
+                            Console.Write("  "); //O ayda olmayan günleri boş geçiyor.
+                        else
+                            Console.Write(tarih[i, j, k] + " ");
+                    }
+
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void istatistikYazdır(int[,] cakisma) //istatistikleri tablo halinde yazdıran metot.
+        {
+            Console.WriteLine("\n\n    İstatistiksel Değerler");
+            Console.WriteLine("\n N>  50\t  100\t  500\t  1000");
+            Console.WriteLine("     -------------------------");
+
+            int [] ortTop = new int[4]; //4 farklı n değeri için ortalamaları tutacak olan dizi.
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i < 9)
+                {
+                    Console.Write(" " + (i + 1) + ".  ");
+                }
+                else { Console.Write((i + 1) + ".  "); }
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(cakisma[j, i] + "\t  ");
+                    ortTop[j]+= cakisma[j, i];
+                }
+                Console.WriteLine();
+            }
+            Console.Write("\nOrt. ");
+            for (int i = 0; i < ortTop.Length; i++)
+            {
+                Console.Write((float)ortTop[i] / 10 + "   ");
+            }
+        }
+    }
+}
---
+++ Program 2.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace proje_1._2
+{
+
+    class Öğrenci //Öğrencilere ait bilgileri içinde barındıran Öğrenci adında class.
+    {
+        static Random r = new Random();
+
+        private string ad;
+        private string soyad;
+        private int dNotu;
+        public Öğrenci() { }
+        public Öğrenci(string ad, string soyad) { this.ad = ad; this.soyad = soyad; dNotu = r.Next(40, 101); }
+        //ad ve soyad'ı parametre alıp dNotu otomatik random atayan constructor.
+        public string adAl() { return ad; }
+        public void adAta(string adı) { ad = adı; }
+        public string soyadAl() { return soyad; }
+        public void soyadAta(string soyadı) { soyad = soyadı; }
+        public int notAl() { return dNotu; }
+
+        public void yazdir()
+        { Console.WriteLine("\t" + ad + " " + soyad + "\t" + dNotu); }
+    }
+
+    class Ülke //Ülkelere ait bilgileri içinde barındıran Ülke adında class.
+    {
+        private string ad;
+        private int kontenjan;
+        public Öğrenci[] ogrDizi;
+        public Ülke() { }
+        public Ülke(string ad) { this.ad = ad; } //ad'ı paramatre olarak alan constructor.
+
+        public string adAl() { return ad; }
+        public int kontenjanAl() { return kontenjan; }
+        public void kontenjanAta(int kont) { kontenjan = kont; }
+    }
+
+    class Program
+    {
+        static Random r = new Random();
+
+        static void Main(string[] args)
+        {
+            int toplamKontenjan;
+            Ülke[] ülkeler = new Ülke[9]; //Ülke tipinde ülkeleri tutan dizi.
+
+            toplamKontenjan = ulkeOlustur(ülkeler); //ulkeOlustur metodu ile daha önce oluşturulan ülkeler dizisi atanıyor. 
+            //Aynı zamanda ülkelerin toplam kontenjanı metottan geliyor.
+            Öğrenci[] öğrenciler;
+            öğrenciler = ogrOlustur(); //ogOlustur metotundan Öğrenci tipinde dizi dönüyor ve öğrenciler dizisine eşitleniyor.
+
+            ogrYerlestir(toplamKontenjan, öğrenciler, ülkeler); //Öğrenciler ülkelerin kontenjanlarına göre yerleştiriliyor.
+
+            ülkeYazdır(ülkeler, toplamKontenjan, öğrenciler); //Yazdırma işlemleri yapılıyor.
+
+            Console.ReadLine();
+        }
+
+        public static int ulkeOlustur(Ülke[] ülkeler)
+        {
+            string[] ülkeİsimleri = { "ENG", "GER", "FRE", "ITA", "ESP", "USA", "JAP", "CHN", "RUS" };
+
+            for (int i = 0; i < 9; i++)
+                ülkeler[i] = new Ülke(ülkeİsimleri[i]);
+
+            int secim, topKota = 0;
+            Console.Write("Ülke kontenjanlarını belirlemek için secenek giriniz (1-Custom / 2-Random): ");
+            secim = sayiAl(1, 2);
+
+            switch (secim)
+            {
+                case 1: //Eğer ülke kontenjanlarını kullanıcı girecekse case 1 çalışıyor.
+                    for (int i = 0; i < 9; i++)
+                    {
+                        Console.Write(ülkeler[i].adAl() + " için kontenjan sayısını (0-10) giriniz: ");
+                        ülkeler[i].kontenjanAta(sayiAl(0, 10));
+                        topKota += ülkeler[i].kontenjanAl();
+                        ülkeler[i].ogrDizi = new Öğrenci[ülkeler[i].kontenjanAl()];
+
+                        for (int j = 0; j < ülkeler[i].kontenjanAl(); j++)
+                            ülkeler[i].ogrDizi[j] = new Öğrenci(); //Ülke sınıfı tipindeki değişkenin içinde bulunun Öğrenci tipindeki dizi için bellekten yer ayrılıyor.
+                    }
+                    break;
+
+                case 2: //Eğer ülke kontenjanları random olarak belirlenecekse case 2 çalışıyor.
+                    for (int i = 0; i < 9; i++)
+                    {
+                        ülkeler[i].kontenjanAta(r.Next(0, 11));
+                        topKota += ülkeler[i].kontenjanAl();
+                        ülkeler[i].ogrDizi = new Öğrenci[ülkeler[i].kontenjanAl()];
+
+                        for (int j = 0; j < ülkeler[i].kontenjanAl(); j++)
+                            ülkeler[i].ogrDizi[j] = new Öğrenci(); //Bellekten yer ayrılıyor.
+                    }
+                    break;
+            }
+            return topKota;
+        }
+
+        public static Öğrenci[] ogrOlustur()
+        {
+            string[] adlar = { "Fatma", "Ahmet", "Ayşe", "Hüseyin", "Emine", "Hasan", "Hatice", "İbrahim", "İsmail", "Osman", "Yusuf", "Murat", "Ömer", "Ramazan", "Halil", "Salih", "Sultan", "Zehra", "Hanife", "Merve" };
+            string[] soyadlar = { "Yıldız", "Sarı", "Öztürk", "Aydın", "Özdemir", "Aslan", "Doğan", "Kılıç",
+                                    "Polat", "Çetin", "Kara", "Koçak", "Kurt", "Özkan", "Şimşek","Çelik","Üstün","Yılmaz","Şahin","Kıral"};
+
+            string[] isimler = new string[400];
+
+            int k = 0; //k değişkeni isimler dizisinin indisini tutuyor.
+            for (int i = 0; i < adlar.Length; i++) //20 ad ve soyad çapraz şekilde karıştırılarak 400 farklı isim oluşturuluyor.
+            {
+                for (int j = 0; j < soyadlar.Length; j++)
+                {
+                    isimler[k] = adlar[i] + " " + soyadlar[j]; //ad ve soyadlar isimler dizisine atanıyor.
+                    k++;
+                }
+            }
+
+            int basvuranOgr;
+            Console.Write("\nBaşvuran öğrenci sayısını (1-150) giriniz: ");
+
+            basvuranOgr = sayiAl(1, 150);
+
+            Öğrenci[] öğrenciler = new Öğrenci[basvuranOgr];
+
+            int altmisUstu = 0; //60 ve üstünde notu olan öğrenci sayısını tutuyor.
+
+            for (int i = 0; i < öğrenciler.Length; i++)
+            {
+                int t;
+                do
+                {
+                    t = r.Next(0, 400);
+                    if (isimler[t] != null)
+                    {
+                        string[] gecici = isimler[t].Split(); //isimler ad ve soyad olarak ayrılıyor.
+                        öğrenciler[i] = new Öğrenci(gecici[0], gecici[1]);
+                    }
+                } while (isimler[t] == null); //isimler dizisinden null olmayanı bulana kadar arıyor.
+
+                Array.Clear(isimler, t, 1); //Kullanılan ismi diziden siliyor.
+                if (öğrenciler[i].notAl() >= 60)
+                    altmisUstu++;
+            }
+
+            Öğrenci[] basarılılar = new Öğrenci[altmisUstu]; //60 ve üstü notu olan öğrencilerin dizisi oluşturuluyor.
+
+            int b = 0; //basarılılar dizisinin indisini tutuyor.
+            for (int i = 0; i < öğrenciler.Length; i++)
+            {
+                if (öğrenciler[i].notAl() >= 60) //60 ve üstü notu olan öğrenciler, basarılılar dizisine alınıyor.
+                {
+                    basarılılar[b] = new Öğrenci();
+                    basarılılar[b] = öğrenciler[i];
+                    b++;
+                }
+            }
+            return basarılılar; //basarılılar dizisi ana programa gönderiliyor.
+        }
+
+        public static int sayiAl(int alt, int ust) //Parametre olarak gelen alt ve üst sınır değerlerine göre integer tipinde değer döndüren metot.
+        {
+            int sayi = Convert.ToInt16(Console.ReadLine());
+
+            while (sayi < alt || sayi > ust)
+            {
+                Console.Write("Hatalı veri girdisi! Girdiyi " + alt + "-" + ust + " aralığında giriniz: ");
+                sayi = Convert.ToInt16(Console.ReadLine());
+            }
+            return sayi;
+        }
+
+        public static void sirala(Öğrenci[] dizi) //Öğrenci tipindeki diziyi öğrencilerin notlarına göre büyükten küçüğe sıralayan metot.
+        {
+            for (int i = 0; i < dizi.Length - 1; i++) //Bubble Sort
+            {
+                for (int j = 1; j < dizi.Length - i; j++)
+                {
+                    if (dizi[j].notAl() > dizi[j - 1].notAl())
+                    {
+                        Öğrenci gecici = dizi[j - 1];
+                        dizi[j - 1] = dizi[j];
+                        dizi[j] = gecici;
+                    }
+                }
+            }
+        }
+
+        public static void ulkeSirala(Ülke[] dizi) //Ülke tipindeki diziyi ülkelerin kontenjan sayılarına göre büyükten küçüğe sıralayan metot.
+        {
+            for (int i = 0; i < dizi.Length - 1; i++) //Buble Sort
+            {
+                for (int j = 1; j < dizi.Length - i; j++)
+                {
+                    if (dizi[j].kontenjanAl() > dizi[j - 1].kontenjanAl())
+                    {
+                        Ülke gecici = dizi[j - 1];
+                        dizi[j - 1] = dizi[j];
+                        dizi[j] = gecici;
+                    }
+                }
+            }
+        }
+
+        public static void ogrYerlestir(int topKota, Öğrenci[] öğrenciler, Ülke[] ülkeler)
+        {
+            int l = 0, k = 0; // l ve k değişkenleri Ülke tipindeki değişkende bulunun ogrDizi ve öğrenciler dizilerinin indislerini tutuyor.
+            if (öğrenciler.Length > topKota) //Eğer yerleşmeye hak kazanan öğrenci sayısı toplam kontenjandan fazlaysa bu blok çalışıyor.
+            {
+                sirala(öğrenciler); //Öğrenciler notlarına göre sıralanır.
+                for (int i = 0; i < ülkeler.Length; i++)
+                {
+                    l = 0; //Her ülke için l değeri 0'lanır.
+                    if (ülkeler[i].kontenjanAl() != 0)
+                    {
+                        while (l < ülkeler[i].kontenjanAl()) //While döngüsü l indisinin ülke kontenjanı sınırına kadar döner.
+                        {
+                            ülkeler[i].ogrDizi[l] = öğrenciler[k];
+                            l++;
+                            k++; //sonraki öğrenci.
+                        }
+                    }
+                }
+            }
+            else //Eğer kontenjan sayısı başarılı öğrencilerin sayısından fazlaysa.
+            {
+                float oran = (float)öğrenciler.Length / topKota; //Eşit yüzdeyle öğrencileri yerleştirmek için belirlenen oran.
+                ulkeSirala(ülkeler); //ülkeler kontenjanlarına göre sıralanır.
+                float gidenOgrSay;
+                for (int i = 0; i < ülkeler.Length; i++)
+                {
+                    l = 0;
+                    if (ülkeler[i].kontenjanAl() != 0)
+                    {
+                        gidenOgrSay = ülkeler[i].kontenjanAl() * oran; //Eşit yüzdeyi sağlamak için yapılan işlem.
+                        while (l < gidenOgrSay)                      //Ülkeye gidecek olan öğrenci sayısını float tipinde belirler.
+                        {
+                            if (k < öğrenciler.Length)
+                            {
+                                ülkeler[i].ogrDizi[l] = öğrenciler[k];
+                                k++; //sonraki öğrenci.
+                            }
+                            l++;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void ülkeYazdır(Ülke[] ülkeler, int topKota, Öğrenci[] öğrenciler)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Kazanan Öğrenciler:");
+            for (int i = 0; i < ülkeler.Length; i++) //Her ülke için(kontenjanı 0 olmayan), o ülkeye giden öğrencilerin listesi yazdırılır.
+            {
+                if (ülkeler[i].kontenjanAl() != 0 && ülkeler[i].ogrDizi[0].adAl() != null)
+                {
+                    int yerlesen = 0;
+                    Console.WriteLine();
+                    for (int j = 0; j < ülkeler[i].kontenjanAl(); j++)
+                    {
+                        if (ülkeler[i].ogrDizi[j].adAl() != null)
+                            yerlesen++;
+                    }
+                    Console.WriteLine(ülkeler[i].adAl() + "> Kontenjan Sayısı: " + ülkeler[i].kontenjanAl()+"\n     Yerleşen Öğrenci Sayısı: "+yerlesen+"\n");
+                    for (int j = 0; j < ülkeler[i].kontenjanAl(); j++)
+                    {
+                        if (ülkeler[i].ogrDizi[j].adAl() != null)
+                            ülkeler[i].ogrDizi[j].yazdir();
+                    }
+                }
+            }
+
+            if (öğrenciler.Length > topKota) //Eğer kazanamayan öğrenci varsa bu blok çalışır.
+            {
+                Console.WriteLine();
+                Console.WriteLine("Kazanamayan Öğrencilerin Sayısı: " + (öğrenciler.Length - topKota));
+                Console.WriteLine();
+                Console.WriteLine("Kazanamayan Öğrenciler:\n");
+                for (int i = topKota; i < öğrenciler.Length; i++)
+                    öğrenciler[i].yazdir();
+            }
+        }
+    }
+}
